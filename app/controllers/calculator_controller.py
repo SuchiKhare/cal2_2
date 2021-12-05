54eb7c60818301025cbf3790280c825afd5969c4
SuchiKhare <sk368.njit.edu>
2021-12-05 16:39:48 -0500

    updated messages , footer

--- app/controllers/calculator_controller.py
+++ app/controllers/calculator_controller.py
@@ -6,29 +6,25 @@ from flask import render_template, request, flash
 class CalculatorController(ControllerBase):
     @staticmethod
     def post():
-        if request.form['value1'] == '' or request.form['value2'] == '':
-            error = 'You must enter a value for value 1 and or value 2'
+        if request.form['input1'] == '' or request.form['input2'] == '':
+            error = 'Please enter a numerical values for input 1 and or input 2'
         else:
-            flash('You successfully calculated')
-            flash('You are awesome')
+            flash('Calculations are successful')
+            flash('Congratulations')
+            flash('Go ahead for next calculations !!')
 
             # get the values out of the form
-            value1 = request.form['value1']
-            value2 = request.form['value2']
+            input1 = request.form['input1']
+            input2 = request.form['input2']
             operation = request.form['operation']
             # make the tuple
-            my_tuple = (value1, value2)
+            my_tuple = (input1, input2)
             # this will call the correct operation
             getattr(Calculator, operation)(my_tuple)
-            result = str(Calculator.get_last_result_value())
-            return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result)
+            my_output = str(Calculator.get_last_result_value())
+            return render_template('result.html', input1=input1, input2=input2, operation=operation, result=my_output)
         return render_template('calculator.html', error=error)
+
     @staticmethod
     def get():
         return render_template('calculator.html')
-
-
-
-
-
-
